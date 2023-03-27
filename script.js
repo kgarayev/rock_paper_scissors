f9ca82c2636bc2c8c8de07d611d87df458e537f8
kgarayev <kenangarayev@gmail.com>
2023-03-27 21:27:59 +0100

    console message based rock paper scissors game finalised

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// Rock Paper Scissors Game - command line interface - no GUI
+
+// declaring constant array
 const weapon = ["rock", "paper", "scissors"];
+
+// declaring an object with the game rules
 const rules = {
   rock: { rock: "draw", scissors: "win", paper: "lose" },
   paper: { paper: "draw", rock: "win", scissors: "lose" },
   scissors: { scissors: "draw", paper: "win", rock: "lose" },
 };
 
+// a function to generate computer's choice (random)
 function getComputerChoice() {
   randomNum = Math.floor(Math.random() * 3);
   console.log("Computer choice is", weapon[randomNum]);
@@ -19,39 +25,40 @@ function getUserInput() {
     userInput = prompt("What is your weapon of choice? ");
   }
 
+  console.log("Your choice is", userInput.toLowerCase());
   return userInput.toLowerCase();
 }
 
-console.log(getUserInput());
-
+// a function that plays one round of the game
 function playRound(playerSelection, computerSelection) {
-  const playerChoice = playerSelection.toLowerCase();
+  // determine the outcome of the game by simply looking up the key-value pair of the object
+  let outcome = rules[playerSelection][computerSelection];
 
-  let outcome = rules[playerChoice][computerSelection];
-
+  // conditional to print the statement
   if (outcome == "win") {
     console.log(
       "You Win! " +
-        playerChoice.toUpperCase() +
+        playerSelection.toUpperCase() +
         " beats " +
         computerSelection.toUpperCase()
     );
-    return outcome;
   } else if (outcome == "lose") {
     console.log(
       "You Lose! " +
         computerSelection.toUpperCase() +
         " beats " +
-        playerChoice.toUpperCase()
+        playerSelection.toUpperCase()
     );
-    return outcome;
   } else {
     console.log("Draw!");
-    return outcome;
   }
+
+  return outcome;
 }
 
+// a function that plays the game 5 times
 function game() {
+  // setting-up an object to keep the tally
   let score = { human: 0, computer: 0 };
 
   for (let i = 0; i < 5; i++) {
@@ -66,22 +73,36 @@ function game() {
     }
   }
 
+  // conditional to print the final statement and return a string of human player's status
   if (score.human > score.computer) {
-    console.log("You have won the game! Congratulations!");
+    console.log(
+      "You beat computer " +
+        score.human +
+        " to " +
+        score.computer +
+        ". You have won the game! Congratulations!"
+    );
     return "win";
   } else if (score.human < score.computer) {
-    console.log("You have lost the game! What a pity...");
+    console.log(
+      "You lose to computer " +
+        score.computer +
+        " to " +
+        score.human +
+        ". You have lost the game! What a pity..."
+    );
     return "lose";
   } else {
     console.log(
-      "The game result is draw. Repeat the game to know who is better."
+      "The score is " +
+        score.human +
+        " to " +
+        score.human +
+        ". The game result is draw. Repeat the game to know who is better."
     );
     return "draw";
   }
 }
 
-// const playerSelection = "ScissORS";
-
-// playRound(playerSelection, getComputerChoice());
-
-// game();
+// play the game
+game();
